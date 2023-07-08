2a483507df31e5614b1b0b1dbde982855bf38a78
hashaam <hashaamkhan4247@gmail.com>
2023-07-08 23:31:53 +0500

    cors error fix

--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -7,6 +7,15 @@ const {
   deleteTask,
 } = require("../controllers/tasksController");
 
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 router.post("/addTask", createTask);
 router.get("/", getTasks);
 router.delete("/:id", deleteTask);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const TaskRoutes = require("./routes/TaskRoutes");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 app.use(express.json());
@@ -8,6 +9,13 @@ const port = process.env.PORT || 3000;
 
 ///////ROUTES///////
 app.use("/tasks", TaskRoutes);
+
+//CORS//
+const corsConfig = {
+  credentials: true,
+  origin: true,
+};
+app.use(cors(corsConfig));
 
 // Connect to MongoDB
 mongoose
